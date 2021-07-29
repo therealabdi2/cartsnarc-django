fe5eb247c00e8542e2d1157e64209305701a3f07
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-07-29 20:52:14 +0500

    add order form and make payments.html

--- cartsnarc/urls.py
+++ cartsnarc/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
                   path('store/', include('store.urls')),
                   path('cart/', include('carts.urls')),
                   path('accounts/', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  # orders
+                  path('orders/', include('orders.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
