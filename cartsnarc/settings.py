90f98dd1e3a8dab50da4b84a55858c70dfdfec95
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-08-02 02:17:09 +0500

    add heroku config

--- cartsnarc/settings.py
+++ cartsnarc/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'cartsnarc.herokuapp.com']
 
 # Application definition
 
