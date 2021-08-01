eaa0c51f9ffedd612274d2b29a27e67a1ef6f15b
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-08-01 15:59:36 +0500

    add review forms and style css

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.store, name='store'),
     path('category/<slug:category_slug>/', views.store, name='products_by_category'),
     path('category<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='products_detail'),
-    path('search/', views.search, name='search')
+    path('search/', views.search, name='search'),
+
 
 ]
