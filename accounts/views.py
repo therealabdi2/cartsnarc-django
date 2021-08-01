a7df5faba56f723fc45d75bcb4176a023c1487a5
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-08-01 20:28:26 +0500

    add my_orders functionality and update urls and views

--- accounts/views.py
+++ accounts/views.py
@@ -241,3 +241,12 @@ def resetPassword(request):
             messages.error(request, "Password do not match")
             return redirect('resetPassword')
     return render(request, 'accounts/resetpassword.html')
+
+
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
+    context = {
+        'orders': orders
+    }
+
+    return render(request, 'accounts/my_orders.html', context)
