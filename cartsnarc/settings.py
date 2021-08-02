562b0bbfaba5e078501075b693aeccdce4bfb681
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-08-02 21:03:48 +0500

    disable compress foe heroku

--- cartsnarc/settings.py
+++ cartsnarc/settings.py
@@ -151,12 +151,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     'cartsnarc/static',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
